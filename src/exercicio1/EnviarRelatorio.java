c2c51c71a9dd22143fd7b087b5f4d8c5e11b17a5
Flavia Hespanhol <flavia.hespanhol@zup.com.br>
2025-01-08 10:42:52 -0300

    feat: implementação classes Relatorio e EnviarRelatorio

---
+++ src/exercicio1/EnviarRelatorio.java
@@ -0,0 +1,8 @@
+package exercicio1;
+
+public class EnviarRelatorio {
+
+    public void enviarPorEmail() {
+        System.out.println("Enviando relatório por e-mail...");
+    }
+}
---
+++ src/exercicio1/Relatorio.java
@@ -0,0 +1,8 @@
+package exercicio1;
+
+public class Relatorio {
+
+    public void gerarRelatorio() {
+        System.out.println("Gerando relatório financeiro...");
+    }
+}
